108b11e2cb1504b834dd7d4ed3bf2d58fa50d25a
marsila <eng.rasha.ali7@gmail.com>
2025-10-03 12:08:43 +0200

    Adding functionality to the buttons

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const rock = document.getElementById('rock');
 const papear = document.getElementById('papear');
-const scissers = document.getElementById('scissers');
+const scissors = document.getElementById('scissors');
 const userSelection = document.getElementById('user-selection');
 // console.log(`user: ${rock}`);
 // function userChoise(event) {
@@ -10,7 +10,7 @@ const userSelection = document.getElementById('user-selection');
 // }
 // rock.addEventListener('input', userChoise);
 // papear.addEventListener('input',userChoise);
-// scissers.addEventListener('input', userChoise);
+// scissors.addEventListener('input', userChoise);
 
 
 
@@ -26,87 +26,73 @@ let computerPoints = 0;
 
 //let computerRandomChoice = () => Math.floor(Math.random()*3 )+1;
 
-function getComputerChoice() {
+let  getComputerChoice = () => {
     let randomNum = Math.floor(Math.random()*3 )+1;
     if (randomNum ===1) 
         return 'Rock';
     else if(randomNum === 2)
         return 'Papear';
-    else return 'Scissers';
+    else return 'Scissors';
 }
 
-
-//function getUserChoice(){
     userSelection.addEventListener('click', (event)=>{
-        let userChoice;
-        const computerSelectedChoice = getComputerChoice();
+       let userChoice;
+       let computerChoice = getComputerChoice();
         if(event.target.tagName === 'BUTTON'){
             console.log(`the clicked one is ${event.target.id}`);
             userChoice = event.target.id;
             output.innerText =`Your choice is : ${userChoice}`;
-            computerChoiceText.innerText = `The computer choice is ${computerSelectedChoice}`;
-        }
+            computerChoiceText.innerText =`Computer choice is : ${computerChoice}`;
+            console.log(`outside if ${userChoice}`);
+            playRound(userChoice, computerChoice) ;
+        }        
     })
-//}
 
-//let x=0;
 
-// papear.addEventListener('click', () => { 
-//     console.log('clicked papear');
-//         //let computerSelectedChoice = getComputerChoice();
-//         //output.innerText =`Your choice is : Paper`;
-//        // computerChoiceText.innerText = `The computer choice is ${computerSelectedChoice}`;
-//         //playRound(selectedChoice, computerSelectedChoice);
-// //  x++;
-// //  if (x >= 5){
-// //     btn.disabled = true; 
-// //     playAgain.innerHTML = `<button onclick= window.location.reload()>Play Agin </button>`
-// // }
-// })
 
-// function playRound(userChoice, computerChoice) {
+function playRound(userChoice, computerChoice) {
+    console.log(`computer = ${computerChoice} user = ${userChoice}`);    
     
-//     if (userChoice ==computerChoice){
-//         console.log('the same');
+    if (userChoice ===computerChoice){
+        console.log('the same');
         
-//     }else {
-//             if (userChoice =='Rock' && computerChoice =='Papear' ){
-//                 computerPoints++;
-//                 console.log(`c+ = ${computerPoints} , u = ${userPoints} `);                
-//             }                
-//             else if (userChoice == 'Rock' && computerChoice == 'Scissers'){
-//                 userPoints++;
-//                 console.log(`c = ${computerPoints} , u+ = ${userPoints} `);
-//             }    
-//             else if (userChoice == 'Scissers' && computerChoice == 'Rock'){
-//                 computerPoints++;
-//                 console.log(`c+ = ${computerPoints} , u = ${userPoints} `);
-//             }
-//             else if (userChoice == 'Scissers' && computerChoice == 'Papear') {
-//                 userPoints++;
-//                 console.log(`c = ${computerPoints} , u+ = ${userPoints} `);
-//             }
-//             else if (userChoice == 'Papear' && computerChoice == 'Rock') {
-//                 userPoints++;
-//                 console.log(`c = ${computerPoints} , u+ = ${userPoints} `);
-//             }
-//             else if (userChoice == 'Papear' && computerChoice == 'Scissers') {
-//                 computerPoints++;
-//                 console.log(`c+ = ${computerPoints} , u = ${userPoints} `);
-//             }
-//     }
-//     console.log(`cf = ${computerPoints} , uf = ${userPoints} `);
+     }else {
+            if (userChoice ==='Rock' && computerChoice =='Papear' ){
+                computerPoints++;
+                console.log(`c+ = ${computerPoints} , u = ${userPoints} `);                
+            }                
+            else if (userChoice === 'Rock' && computerChoice == 'Scissors'){
+                userPoints++;
+                console.log(`c = ${computerPoints} , u+ = ${userPoints} `);
+            }    
+            else if (userChoice === 'Scissors' && computerChoice == 'Rock'){
+                computerPoints++;
+                console.log(`c+ = ${computerPoints} , u = ${userPoints} `);
+            }
+            else if (userChoice === 'Scissors' && computerChoice == 'Papear') {
+                userPoints++;
+                console.log(`c = ${computerPoints} , u+ = ${userPoints} `);
+            }
+            else if (userChoice === 'Papear' && computerChoice == 'Rock') {
+                userPoints++;
+                console.log(`c = ${computerPoints} , u+ = ${userPoints} `);
+            }
+            else if (userChoice === 'Papear' && computerChoice == 'Scissors') {
+                computerPoints++;
+                console.log(`c+ = ${computerPoints} , u = ${userPoints} `);
+            }
+    }
+    console.log(`cf = ${computerPoints} , uf = ${userPoints} `);
     
-//     userResultText.innerText =`${userPoints}`;
-//     computerResultText.innerText = `${computerPoints}`;
+    userResultText.innerText =`${userPoints}`;
+    computerResultText.innerText = `${computerPoints}`;
     
-//     if (userPoints > computerPoints) {
-//       finalResultText.innerText = `The winner is the User!`;  
-//     } else if (userPoints < computerPoints){
-//       finalResultText.innerText = `The winner is the Computer!` ; 
-//     } else {finalResultText.innerText =`It's tight!`}
+    if (userPoints > computerPoints) {
+      finalResultText.innerText = `The winner is the User!`;  
+    } else if (userPoints < computerPoints){
+      finalResultText.innerText = `The winner is the Computer!` ; 
+    } else {finalResultText.innerText =`It's tight!`}
 
-// }
-
+}
 
 
